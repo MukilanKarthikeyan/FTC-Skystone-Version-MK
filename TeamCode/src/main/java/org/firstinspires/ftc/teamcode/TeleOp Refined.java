061c9be31a1698036f62083c6edde21b441b0630
Mukilan Karthikeyan <47691192+MukilanKarthikeyan@users.noreply.github.com>
2020-01-14 20:32:11 -0500

    First Update
    Updated the TeleOp code for the slides so that it directly sets the power of the motor to the input vlaue form the controller

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp Refined.java
@@ -0,0 +1,137 @@
+package org.firstinspires.ftc.teamcode;
+//Importing the hardware classes used in program
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.SampleRevBlinkinLedDriver;
+import org.firstinspires.ftc.robotcore.internal.system.Deadline;
+
+import java.util.concurrent.TimeUnit;
+
+@TeleOp
+public class TeleOpTest extends LinearOpMode {
+
+    //HardwareBot robot;
+    double left;
+    double right;
+    double drive;
+    double turn;
+    double max;
+
+    boolean wentUpOnce = false;
+
+    DcMotor leftFront = null;
+    DcMotor rightFront = null;
+    DcMotor leftBack;
+    DcMotor rightBack;
+
+    //CRServo crservo = null;
+
+    DcMotor slides = null;
+
+    Servo clutch = null;
+    double up;
+    double down;
+    Servo autoClutch = null;
+
+    Deadline gamepadRateLimit;
+    private final int GAMEPAD_LOCKOUT = 500;
+
+    int numClickedUp = -1;
+    int numClickedDown = -1;
+
+    //double newTarget;
+    double counts;
+
+    double WHEEL_DIAMETER_INCHES = 4.0;
+    double COUNTS_PER_MOTOR_REV = 1680;
+    double GEAR_REDUCTION = 1.5; //should be 1.5
+
+
+
+    public void runOpMode() throws InterruptedException {
+        //robot = new HardwareBot(this);
+
+        /*Initializing the drivetrain motors*/
+        leftFront = hardwareMap.get(DcMotor.class, "lf");
+        rightFront = hardwareMap.get(DcMotor.class, "rf");
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        leftBack = hardwareMap.get(DcMotor.class, "lb");
+        rightBack = hardwareMap.get(DcMotor.class, "rb");
+        rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        /*Initializing the motor for the slides*/
+        slides = hardwareMap.dcMotor.get("slides");
+        slides.setDirection(DcMotorSimple.Direction.REVERSE);
+        slides.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        slides.setMode(DcMotor.RunMode.RUN_USING_ENCODER); //need to change to RUN_USING_ENCODER
+
+        /*Initializing the clutch servos*/
+        clutch = hardwareMap.get(Servo.class,"clutch");
+        autoClutch = hardwareMap.servo.get("autoClutch");
+
+        gamepadRateLimit = new Deadline(GAMEPAD_LOCKOUT, TimeUnit.MILLISECONDS);
+
+        waitForStart();
+
+        while (!isStopRequested()) {
+
+            drive = -gamepad1.left_stick_y;
+            turn = gamepad1.right_stick_x;
+
+            left = drive - turn;
+            right = drive + turn;
+
+            max = Math.max(Math.abs(left), Math.abs(right));
+            if (max > 1.0) {
+                left /= max;
+                right /= max;
+            }
+
+            /*SET UP SLIDES*/
+            up = gamepad1.left_trigger + gamepad2.left_trigger;
+            down = gamepad1.right_trigger + gamepad2.right_trigger;
+
+            if(up > 0.1) {
+                slides.setPower(up);
+
+            }
+            else if(down > 0.1) {
+                slides.setPower(-down); // the input values are positive so need to add negative
+
+            }
+            else {
+                slides.setPower(0);
+            }
+
+            leftFront.setPower(left);
+            leftBack.setPower(left);
+            rightFront.setPower(right);
+            rightBack.setPower(right);
+
+
+            if(gamepad1.dpad_up || gamepad2.dpad_up) {
+                clutch.setPosition(0.2); // position clutch to collect stone
+
+            }
+            else if(gamepad1.dpad_down || gamepad2.dpad_down) {
+                clutch.setPosition(0.7);// position clutch to deliver stone
+            }
+
+            if(gamepad1.a || gamepad2.a) {
+                autoClutch.setPosition(1);
+            }
+
+            if(gamepad1.x || gamepad2.x) {
+                autoClutch.setPosition(0.4);
+            }
+
+        }
+    }
+
+}
