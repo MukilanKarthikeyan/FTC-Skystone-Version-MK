e91816ba16aac4dd9f77c4f309a0fca3653eb4b8
Mukilan <mukilankarthikeyan357@gmail.com>
2020-08-03 16:34:32 -0400

    added the drive forward method [fwd(int rev)] to run using encoder postion -added a private final variable to measue the ticsk and used to calculate the dista e run.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumAuto.java
@@ -6,8 +6,8 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
@@ -26,17 +26,56 @@ public class MecanumAuto extends LinearOpMode {
     BNO055IMU imu;
     Orientation angles;
 
+    private final int TICKS = 1120;
 
+    @Override
     public void runOpMode() throws InterruptedException {
-            leftFront = hardwareMap.get(DcMotor.class, "lf");
-            rightFront = hardwareMap.get(DcMotor.class, "rf");
-            rightFront.setDirection(DcMotor.Direction.REVERSE);
-            leftBack = hardwareMap.get(DcMotor.class, "lb");
-            rightBack = hardwareMap.get(DcMotor.class, "rb");
-            rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
-    }
-    public void fwd(int dist){
+        //assign each motor
+        leftFront = hardwareMap.dcMotor.get("lf");
+        rightFront = hardwareMap.dcMotor.get("rf");
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        leftBack = hardwareMap.dcMotor.get("lb");
+        rightBack = hardwareMap.dcMotor.get("rb");
+        rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        waitForStart();
+        fwd(1);
+    }
+    public void fwd(int rev){
+        int dist = rev*TICKS;
+        //Stop and Reset the encoders
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        //rev is the number of revolutions of the motor(need to change it to distance in meters/inches so it will calculate revs)
+        //set the distace goal
+        leftFront.setTargetPosition(dist);
+        leftBack.setTargetPosition(dist);
+        rightFront.setTargetPosition(dist);
+        rightBack.setTargetPosition(dist);
+
+        //change to run to position mode
+        leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        //set power to motors
+        DrivePower(0.3);
+
+        while (leftFront.isBusy() && leftBack.isBusy() && rightFront.isBusy() && rightBack
+                .isBusy()) {
+        }
+
+        DrivePower(0.0);
+    }
+    public void DrivePower(double pow){
+        leftFront.setPower(pow);
+        leftBack.setPower(pow);
+        rightFront.setPower(pow);
+        rightBack.setPower(pow);
     }
     public void rever(int dist){
 
